7957dc2eb1a8307472257f867d9f1ed83d94092d
Jorge Miranda <jmiranda32691@gmail.com>
2026-02-11 00:03:53 -0600

    deleteActivity from the api is set

--- src/activities/ActivityList.jsx
+++ src/activities/ActivityList.jsx
@@ -1,8 +1,32 @@
+import { useAuth } from "../auth/AuthContext";
+import { deleteActivity } from "../api/activities";
+
 export default function ActivityList({ activities }) {
+  const { token } = useAuth();
+
+
+
+
+  async function tryDelete(){
+    
+
+      await deleteActivity(token, activities.id )
+
+
+  if (!token) {
+    return <p>You must be logged in to view activities.</p>;
+  }
+  
   return (
     <ul>
       {activities.map((activity) => (
-        <li key={activity.id}>{activity.name}</li>
+        <>
+        <li key={activity.id}>
+        <p>{activity.name}</p>
+        {token && <button onClick={tryDelete}>Delete</button>}
+        </li>
+        </>
+            
       ))}
     </ul>
   );
--- src/api/activities.js
+++ src/api/activities.js
@@ -12,6 +12,33 @@ export async function getActivities() {
   }
 }
 
+export async function  deleteActivity(token,id){
+  if(!token){
+    throw Error('You must be signed it to delete activites.')
+  }
+  try{
+    const res = await fetch(API + 'activities/'+ id,{
+      method: 'DELETE',
+      headers: {
+        'Content-type': 'application/json',
+        Authorization: 'Bearer' + token
+      }
+    });
+    if(!res.ok){
+      const result = res.json();
+      throw Error(result.message)
+    }
+    
+
+  }catch(e){
+    console.log(e);
+    return [];
+
+  }
+}
+
+
+
 /**
  * Sends a new activity to the API to be created.
  * A valid token is required.
